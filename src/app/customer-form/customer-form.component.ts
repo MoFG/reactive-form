f2f8b8981f45e9d791f8f53888c531f9c5ee2095
Hoa Nguyen <skynguyen79@gmail.com>
2018-07-11 10:21:14 +0700

    ccc

--- src/app/customer-form/customer-form.component.ts
+++ src/app/customer-form/customer-form.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {  FormBuilder,
-          FormGroup,
-          FormControl,
-          Validators,
-          ValidatorFn } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+  ValidatorFn
+} from '@angular/forms';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -15,32 +17,32 @@ export class CustomerFormComponent implements OnInit {
 
   pattern = /^[^*|":<>[\]{}.,?/`~¥£€\\()';@&$!#%^*_+=0-9-]+$/;
 
-  customerForm:     FormGroup;
-  firstName:        FormControl;
-  lastName:         FormControl;
-  middleName:       FormControl;
-  gender:           FormControl;
-  occupation:       FormControl;
-  dob:              FormControl;
+  customerForm: FormGroup;
+  firstName: FormControl;
+  lastName: FormControl;
+  middleName: FormControl;
+  gender: FormControl;
+  occupation: FormControl;
+  dob: FormControl;
 
+  maxDate = new Date();
+  minDate = new Date(
+    new Date().getFullYear() - 100,
+    new Date().getMonth(),
+    new Date().getDate() + 1
+  );
+
+  selectedYear: number;
+  result: number;
+  _flag: boolean = true;
 
   constructor(private fb: FormBuilder) {
-    // this.customerForm = this.fb.group({
-    //   firstName: new FormControl('', [Validators.required, Validators.pattern(this.pattern)]),
-    //   lastName: new FormControl('', [Validators.required, Validators.pattern(this.pattern)]),
-    //   middleName: new FormControl('', [Validators.required, Validators.pattern(this.pattern)]),
-    //   gender: new FormControl('', Validators.required),
-    //   occupation: new FormControl('', Validators.required),
-    //   dob: new FormControl('', Validators.required)
-    // });
     this.firstName = new FormControl('', [Validators.required, Validators.pattern(this.pattern)]);
     this.lastName = new FormControl('', [Validators.required, Validators.pattern(this.pattern)]);
     this.middleName = new FormControl('', [Validators.required, Validators.pattern(this.pattern)]);
     this.gender = new FormControl('', Validators.required);
     this.occupation = new FormControl('', [Validators.required, Validators.pattern(this.pattern)]);
     this.dob = new FormControl('', Validators.required);
-    // this.dob.setValidators
-
 
     this.customerForm = this.fb.group({
       firstName: this.firstName,
@@ -54,8 +56,21 @@ export class CustomerFormComponent implements OnInit {
   }
 
   ngOnInit() {
-    console.log(this.dob.value);
-    
+  }
+
+  enterDOB(event: any) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  selectDOB(event: any) {
+    this.selectedYear = event.value.getFullYear();
+    this.result = this.maxDate.getFullYear() - this.selectedYear;
+    if (this.result < 18) {
+      return this._flag = false;
+    } else {
+      return this._flag = true;
+    }
   }
 
 }
