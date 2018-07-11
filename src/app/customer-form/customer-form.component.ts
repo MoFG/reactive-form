12a4da7d5b1a0c5acd23df92956d7940460991a5
IMT-SOFT\hoanv <skynguyen79@gmail.com>
2018-07-11 07:23:57 +0700

    bbb

--- src/app/customer-form/customer-form.component.ts
+++ src/app/customer-form/customer-form.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import {  FormBuilder,
+          FormGroup,
+          FormControl,
+          Validators,
+          ValidatorFn } from '@angular/forms';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -10,28 +14,48 @@ import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, FormControl, 
 export class CustomerFormComponent implements OnInit {
 
   pattern = /^[^*|":<>[\]{}.,?/`~¥£€\\()';@&$!#%^*_+=0-9-]+$/;
-  customerForm: FormGroup;
-  firstName: FormControl;
-  lastName: FormControl;
-  middleName: FormControl;
-  gender: FormControl;
-  occupation: FormControl;
-  dob: FormControl;
+
+  customerForm:     FormGroup;
+  firstName:        FormControl;
+  lastName:         FormControl;
+  middleName:       FormControl;
+  gender:           FormControl;
+  occupation:       FormControl;
+  dob:              FormControl;
 
 
   constructor(private fb: FormBuilder) {
+    // this.customerForm = this.fb.group({
+    //   firstName: new FormControl('', [Validators.required, Validators.pattern(this.pattern)]),
+    //   lastName: new FormControl('', [Validators.required, Validators.pattern(this.pattern)]),
+    //   middleName: new FormControl('', [Validators.required, Validators.pattern(this.pattern)]),
+    //   gender: new FormControl('', Validators.required),
+    //   occupation: new FormControl('', Validators.required),
+    //   dob: new FormControl('', Validators.required)
+    // });
+    this.firstName = new FormControl('', [Validators.required, Validators.pattern(this.pattern)]);
+    this.lastName = new FormControl('', [Validators.required, Validators.pattern(this.pattern)]);
+    this.middleName = new FormControl('', [Validators.required, Validators.pattern(this.pattern)]);
+    this.gender = new FormControl('', Validators.required);
+    this.occupation = new FormControl('', [Validators.required, Validators.pattern(this.pattern)]);
+    this.dob = new FormControl('', Validators.required);
+    // this.dob.setValidators
+
+
     this.customerForm = this.fb.group({
-      firstName: new FormControl('', [Validators.required, Validators.pattern(this.pattern)]),
-      lastName: new FormControl('', [Validators.required, Validators.pattern(this.pattern)]),
-      middleName: new FormControl('', [Validators.required, Validators.pattern(this.pattern)]),
-      gender: new FormControl('', Validators.required),
-      occupation: new FormControl('', Validators.required),
-      dob: new FormControl('', Validators.required)
+      firstName: this.firstName,
+      middleName: this.middleName,
+      lastName: this.lastName,
+      gender: this.gender,
+      occupation: this.occupation,
+      dob: this.dob
     });
+
   }
 
   ngOnInit() {
+    console.log(this.dob.value);
+    
   }
-
 
 }
